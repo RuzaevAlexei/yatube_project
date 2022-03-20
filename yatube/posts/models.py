b6298a681fb71a24a3d790e8ec7e9c74c620dd40
JustWork Robot <robot@just-work.org>
2022-03-20 20:31:20 +0300

    Проблемы с миграциями поправил.

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,11 +6,10 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.TextField(max_length=200)
-    sslug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True)
     description = models.TextField()
 
 
-    
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
